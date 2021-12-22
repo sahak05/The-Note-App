9a4e19a30084b735c3b51593bc265531a29902ef
sahak05 <61946862+sahak05@users.noreply.github.com>
2021-12-21 21:34:55 -0500

    starting looking good

--- thenoteapp/src/pages/NotePage.js
+++ thenoteapp/src/pages/NotePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import notes from '../assets/data'
-
+import {Link} from 'react-router-dom'
+import {ReactComponent as ArrowLeft} from '../assets/arrow-left.svg'
 
 //we distructure the match object at the place of props
 const NotePage = (props) => {
@@ -9,8 +10,17 @@ const NotePage = (props) => {
     let note =notes.find(note => note.id===Number(noteId))
     
     return (
-        <div>
-            <p>{note?.body}</p>
+        <div className='note'>
+            <div className='note-header'>
+                <h3>
+                    <Link to="/">
+                        <ArrowLeft/>
+                    </Link>
+                </h3>
+            </div>
+            <textarea value={note?.body}>
+
+            </textarea>
         </div>
     )
 }
--- thenoteapp/src/pages/NotesListPage.js
+++ thenoteapp/src/pages/NotesListPage.js
@@ -3,7 +3,11 @@ import notes from '../assets/data'
 import ListItem from '../components/ListItem'
 const NotesListPage = () => {
     return (
-        <div>
+        <div className='notes'>
+            <div className='notes-header'>
+                <h2 className='notes-title'>&#9782; Notes</h2>
+                <p className='note-count'>{notes.length}</p>
+            </div>
             <div className="notes-list">
                 {notes.map((note, index) => (
                     <ListItem  key={index} note={note}/>
